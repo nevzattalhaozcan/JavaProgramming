8c8f6042f5d71f2e7ea83e22b4ea738bf30520ac
Nevzat Talha Ozcan <ozcann.talha@gmail.com>
2022-12-14 19:39:23 +0300

    ParkingLot.java has been created

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,14 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Camry", 2022, 20000,"blue",100);
+
+        Tesla tesla = new Tesla("Model S",2021,30000,"black",1000);
+
+        BMW bmw = new BMW("i5",2019,33000,"white",150);
+
+    }
+}
