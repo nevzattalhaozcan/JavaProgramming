c28b6e90b9b7025cbe6a36b0ebecbde57e452b4b
Nevzat Talha Ozcan <ozcann.talha@gmail.com>
2022-12-14 19:13:16 +0300

    Car.java, Toyota.java, BMW.java, and Tesla.java have been moved to carTask package

--- src/day38_Inheritance/carTask/Toyota.java
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -1,6 +1,8 @@
-package day38_Inheritance;
+package day38_Inheritance.carTask;
 
-public class Toyota extends Car{
+import day38_Inheritance.carTask.Car;
+
+public class Toyota extends Car {
 
     public Toyota(String model, int year, double price, String color, int miles) {
         super("Toyota", model, year, price, color, miles);
