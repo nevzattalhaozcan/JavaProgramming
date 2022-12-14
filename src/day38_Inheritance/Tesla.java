585215e3b0decff66bf8829223f58b8a3e07df15
Nevzat Talha Ozcan <ozcann.talha@gmail.com>
2022-12-14 19:10:20 +0300

    Tesla.java has been created

---
+++ src/day38_Inheritance/Tesla.java
@@ -0,0 +1,16 @@
+package day38_Inheritance;
+
+public class Tesla extends Car{
+
+    public Tesla(String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+    }
+
+    public void autoPilot(){
+        System.out.println(brand + " " + model + " is on auto-pilot.");
+    }
+
+}
+/*3. Tesla:
+					extra methods:
+						autoPilot()*/
