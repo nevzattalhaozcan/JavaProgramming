f97ba601bd6444bbf3ae2fc17e2b369b47f7027e
Nevzat Talha Ozcan <ozcann.talha@gmail.com>
2022-12-14 20:14:07 +0300

    start() method of Car class has been overridden

--- src/day38_Inheritance/carTask/Toyota.java
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -12,6 +12,11 @@ public class Toyota extends Car {
         System.out.println(brand + " " + model + " is reliable.");
     }
 
+    @Override
+    public void start(){
+        System.out.println("Twist the key to ignition to start " + brand + " " + model);
+    }
+
 }
 /*	1. Toyota:
 					extra methods:
