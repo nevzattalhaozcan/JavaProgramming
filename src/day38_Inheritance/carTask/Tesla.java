1c21242bc76cfb0ff9f459eebf16b8b2bd805246
Nevzat Talha Ozcan <ozcann.talha@gmail.com>
2022-12-14 20:14:28 +0300

    start() method of Car class has been overridden

--- src/day38_Inheritance/carTask/Tesla.java
+++ src/day38_Inheritance/carTask/Tesla.java
@@ -12,6 +12,11 @@ public class Tesla extends Car {
         System.out.println(brand + " " + model + " is on auto-pilot.");
     }
 
+    @Override
+    public void start(){
+        System.out.println("Say \"Start\" to start " + brand + " " + model);
+    }
+
 }
 /*3. Tesla:
 					extra methods:
