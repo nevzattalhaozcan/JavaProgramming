49d7f27bedfa8793311e85615bd45852a2a82162
Nevzat Talha Ozcan <ozcann.talha@gmail.com>
2022-12-16 00:34:29 +0300

    Rectangle.java has been created

---
+++ src/day39_Recap/shapeTask/Rectangle.java
@@ -0,0 +1,57 @@
+package day39_Recap.shapeTask;
+
+public class Rectangle extends Shape{
+
+    private double length, width;
+
+    public Rectangle(double length, double width) {
+        super("Rectangle");
+        setLength(length);
+        setWidth(width);
+    }
+
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if(length <= 0){
+            System.err.println("Invalid length: " + length);
+            System.exit(1);
+        }
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if(width <= 0){
+            System.err.println("Invalid width: " + width);
+            System.exit(1);
+        }
+        this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+
+    @Override
+    public double perimeter() {
+        return (length + width) * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                " Length: " + length +
+                ", Width:" + width +
+                ", Area: " + area() +
+                ", Perimeter: " + perimeter() +
+                '}';
+    }
+}
