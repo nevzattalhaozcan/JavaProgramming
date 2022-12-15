b585a4ea5f1522adad3481e0b9e4868376d483f8
Nevzat Talha Ozcan <ozcann.talha@gmail.com>
2022-12-16 00:27:34 +0300

    Square.java has been created

---
+++ src/day39_Recap/shapeTask/Square.java
@@ -0,0 +1,42 @@
+package day39_Recap.shapeTask;
+
+public class Square extends Shape{
+
+    private double side;
+
+    public Square(double side) {
+        super("Square");
+        setSide(side);
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if(side <= 0){
+            System.err.println("Invalid Side: " + side);
+            System.exit(1);
+        }
+        this.side = side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square {" +
+                " Side: " + side +
+                ", Area: " + area() +
+                ", Perimeter: " + perimeter() +
+                '}';
+    }
+
+    @Override
+    public double area() {
+        return side * side;
+    }
+
+    @Override
+    public double perimeter() {
+        return side * 4;
+    }
+}
