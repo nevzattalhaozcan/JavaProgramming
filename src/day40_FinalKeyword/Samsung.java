3f542a4c6bed588abb4d793498465cbfeac022c6
nevzattalhaozcan <ozcann.talha@gmail.com>
2022-12-21 14:27:04 +0300

    Created Samsung.java

---
+++ src/day40_FinalKeyword/Samsung.java
@@ -0,0 +1,13 @@
+package day40_FinalKeyword;
+
+//public class Samsung extends IPhone{ // Gives compiler error
+//
+//    public Samsung(String brand, String model, String color, String size, double price) {
+//        super(brand, model, color, size, price);
+//    }
+//
+//}
+
+// IPhone class is final. So, it cannot be inherited.
+
+// ***Every method can be overloaded regardless of whether it is final or not.
