eb8d1cab745303e4ec85721d9f608f921874a85f
nevzattalhaozcan <ozcann.talha@gmail.com>
2022-12-21 13:39:41 +0300

    Created ProtectedAccessModifier.java

---
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,15 @@
+package day40_FinalKeyword;
+
+public class ProtectedAccessModifier {
+
+    // default
+    static String name1 = "Cydeo";
+
+    // protected
+    protected static String name2 = "WoodenSpoon";
+
+}
+/*
+Protected: Visible in the same package
+           Visible outside the package in subclass
+ */
