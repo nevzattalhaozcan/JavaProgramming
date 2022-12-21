087e16dc681db402c1fd4ac97901e5d3bd6135ad
nevzattalhaozcan <ozcann.talha@gmail.com>
2022-12-21 13:40:03 +0300

    Created AccessModifiersTest2.java

---
+++ src/utilities/AccessModifiersTest2.java
@@ -0,0 +1,14 @@
+package utilities;
+
+import day40_FinalKeyword.ProtectedAccessModifier;
+
+public class AccessModifiersTest2 extends ProtectedAccessModifier{ // inheritance
+
+    public static void main(String[] args) {
+
+//        System.out.println(ProtectedAccessModifier.name1);
+
+        System.out.println(ProtectedAccessModifier.name2);
+
+    }
+}
