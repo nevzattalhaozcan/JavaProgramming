43a733c8609955bb5a3fa9f0f5f03e9424729969
nevzattalhaozcan <ozcann.talha@gmail.com>
2022-12-28 14:44:11 +0300

    Created Developer.java

---
+++ src/day43_Abstraction/employee/Developer.java
@@ -0,0 +1,23 @@
+package day43_Abstraction.employee;
+
+public final class Developer extends Employee{
+
+    public Developer(String name, int age, char gender, int id, String jobTitle, double salary) {
+        super(name, age, gender, id, jobTitle, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getName() + " is coding");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println(getName() + " sleeps 7 hours");
+    }
+
+    public void unitTest(){
+        System.out.println(getName() + " is unit testing");
+    }
+
+}
