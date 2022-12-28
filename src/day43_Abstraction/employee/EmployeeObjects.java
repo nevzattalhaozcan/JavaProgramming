80d700a4917836eed0e20b4e429b857450cb3743
nevzattalhaozcan <ozcann.talha@gmail.com>
2022-12-28 14:54:33 +0300

    Created EmployeeObjects.java

---
+++ src/day43_Abstraction/employee/EmployeeObjects.java
@@ -0,0 +1,17 @@
+package day43_Abstraction.employee;
+
+public class EmployeeObjects {
+
+    public static void main(String[] args){
+
+//        Person person = new Person(); abstract class
+
+//        Employee employee = new Employee(); abstract class
+
+        Tester tester = new Tester("Nevzat", 26,'M',1,"QA",100000);
+
+
+
+    }
+
+}
